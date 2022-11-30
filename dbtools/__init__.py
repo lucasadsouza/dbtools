4f329c9307249d41d14c0b0f031dcdecbd0bfb89
lucasadsouza <lucasadsouza.dev@gmail.com>
2022-11-29 22:16:06 -0300

    Changed module version on the main __init__.py file (1.0.1)

--- dbtools/__init__.py
+++ dbtools/__init__.py
@@ -9,7 +9,7 @@ __project__ = 'dbtools'
 __author__ = 'lucasadsouza'
 __authoremail__ = 'lucasadsouza.dev@gmail.com'
 __url__ = 'https://github.com/lucasadsouza/dbtools'
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 
 import dbtools.databases
 import dbtools.querybuilder
