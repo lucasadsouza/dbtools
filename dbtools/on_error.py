329867b6ff6f06412a4a1ec120cb68198bd40f55
lucasadsouza <lucasadsouza.dev@gmail.com>
2022-12-08 18:47:47 -0300

    Reformulated on_error function to allow the user handle better the exceptions raised by this module (1.0.6)

--- dbtools/on_error.py
+++ dbtools/on_error.py
@@ -12,13 +12,8 @@ def on_error(exceptions: Exception):
         for error in exceptions:
           if issubclass(error, type(e)):
             error = error(str(e))
-            tb = sys.exc_info()[2]
-            message = traceback.format_exception_only(type(error), error)[0]
 
-            traceback_message = traceback.format_tb(tb)[-1]
-            traceback_message = f'{traceback_message}\n    {message}'
-
-            print(traceback_message)
+      raise error
 
     return wrapped
 
